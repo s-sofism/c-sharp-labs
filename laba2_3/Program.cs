76852d1d2fd63bc357321755d382afc5a6c1480e
soff <79422463+s-sofism@users.noreply.github.com>
2021-03-11 11:10:22 +0300

    Update Program.cs

--- laba2_3/Program.cs
+++ laba2_3/Program.cs
@@ -7,53 +7,63 @@ namespace laba2_3
         static void Main(string[] args)
         {
             bool run = true;
-            int a = 0;
-            int b = 0;
+            string a;
+            string b;
+            uint firstNumber = 0;
+            uint lastNumber = 0;
             while (run)
             {
                 Console.WriteLine("enter value a");
-                a = Convert.ToInt32(Console.ReadLine());
+                a = Console.ReadLine();
                 Console.WriteLine("enter value b");
-                b = Convert.ToInt32(Console.ReadLine());
-                if (a > b)
+                b = Console.ReadLine();
+                if (UInt32.TryParse(a, out firstNumber) && UInt32.TryParse(b, out lastNumber))
+                {
+                    if (firstNumber >= lastNumber)
+                    {
+                        Console.WriteLine("try again \n");
+                        run = true;
+                    }
+                    else
+                    {
+                        run = false;
+                    }
+                }
+                else
                 {
                     Console.WriteLine("try again \n");
                     run = true;
                 }
-                else
+            }
+            if (lastNumber != 0 && firstNumber != 0)
+            {
+                uint resultDegree;
+                uint maxDegree = 0;
+                uint number = 1;
+                uint result = 1;
+                while (result != 0)
                 {
-                    run = false;
+                    number = number * 2;
+                    result = lastNumber / number;
+                    maxDegree = maxDegree + result;
                 }
-            }
-            int resultDegree;
-            int maxDegree = 0;
-            int number = 1;
-            int result = 1;
-            while (result != 0)
-            {
-                number = number * 2;
-                result = b / number;
-                maxDegree = maxDegree + result;
-            }
-            int minDegree = 0;
-            number = 1;
-            result = 1;
-            a = a - 1;
-            while (result != 0)
-            {
-                number = number * 2;
-                result = a / number;
-                minDegree = minDegree + result;
-            }
-            if (a + 1 <= 0)
-            {
-                resultDegree = 0;
+                uint minDegree = 0;
+                number = 1;
+                result = 1;
+                firstNumber = firstNumber - 1;
+                while (result != 0)
+                {
+                    number = number * 2;
+                    result = firstNumber / number;
+                    minDegree = minDegree + result;
+                }
+                resultDegree = maxDegree - minDegree;
+                Console.WriteLine("the maximum power of two is " + resultDegree);
             }
             else
             {
-                resultDegree = maxDegree - minDegree;
+                Console.WriteLine("the maximum power of two is 0");
             }
-            Console.WriteLine("the maximum power of two is " + resultDegree);
         }
     }
 }
